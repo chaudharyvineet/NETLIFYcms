6231f79ba048fdae2e10eb82da39c09756e63d27
Vineet Chaudhary <vineetchaudhary131@gmail.com>
2018-05-22 17:26:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
   <div> 
   <h1 align="center"> All Rights Reserved &copy; PATH_MONEY 2018 </h1>
   <hr/>
-  <div align="center>"<a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/pinterst.svg" width="30px" /> </a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/Twitter.svg" width="30px" /></a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/Mail.svg" width="30px" /></a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/G+.svg" width="30px" /></a></div>
+  <div align="center"><a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/pinterst.svg" width="30px" /> </a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/Twitter.svg" width="30px" /></a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/Mail.svg" width="30px" /></a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/G+.svg" width="30px" /></a></div>
   </div>
   </footer>
 )
