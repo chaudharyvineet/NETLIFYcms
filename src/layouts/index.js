b3755b7ff9a50578a52dd7310e5d6eedf97560cf
Vineet Chaudhary <vineetchaudhary131@gmail.com>
2018-05-22 16:12:10 +0530

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
