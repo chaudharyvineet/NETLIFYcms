90e847f0eac2e081af9d6e6b987eb6beb345a7ab
Vineet Chaudhary <vineetchaudhary131@gmail.com>
2018-05-22 16:49:11 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Footer = () => (
-  <footer>
+  <footer className="footer">
   <div> 
   <h1> All Rights Reserved &copy; PATH_MONEY 2018 </h1>
   </div>
