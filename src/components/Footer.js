c3b544a1a61057d24c59a99d563eed9c70dd5c08
Vineet Chaudhary <vineetchaudhary131@gmail.com>
2018-05-22 16:24:50 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,7 @@ import Link from 'gatsby-link'
 const Footer = () => (
   <footer>
   <div> 
-  <h1>pAth mOney &copy; 2018 
-  <br>
-  All Rights Reserved
-  </h1>
+  <h1> pAth mOney &copy; 2018 All Rights Reserved </h1>
   </div>
   </footer>
 )
