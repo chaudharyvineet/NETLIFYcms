a8a4082178b2f07721a3f3bc210ed080daaf28a7
Vineet Chaudhary <vineetchaudhary131@gmail.com>
2018-05-22 17:13:06 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,11 @@ import Link from 'gatsby-link'
 const Footer = () => (
   <footer className="footer">
   <div> 
-  <h1> All Rights Reserved &copy; PATH_MONEY 2018 </h1>
+  <h1 align="center"> All Rights Reserved &copy; PATH_MONEY 2018 </h1>
+  <hr/>
+  <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/pinterst.svg" width="30px"> </a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/Twitter.svg" width="30px"></a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/Mail.svg" width="30px"></a> <a href="https://nakultanta.github.io/pathmoney/index.html"><img src="https://nakultanta.github.io/pathmoney/svg/G+.svg" width="30px"></a>
+
+<hr/>
   </div>
   </footer>
 )
