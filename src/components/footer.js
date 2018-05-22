6431d430cc9b08529e55d16b8033f2d49c65f4ca
Vineet Chaudhary <vineetchaudhary131@gmail.com>
2018-05-22 16:16:48 +0530

    Create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = () => (
+  <footer>
+  <div> 
+  <h1>pAth mOney &copy; 2018 
+  <br>
+  All Rights Reserved
+  </h1>
+  </div>
+  </footer>
+)
+
+export default Footer
